f16155197af4bbc6e932b2308efcfe858d3e4ec9
Rudra Pratap Singh <rpsingh7672@gmail.com>
2026-02-02 22:33:49 +0530

    Update guess.c

--- guess.c
+++ guess.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main() {
+int main(){
     int random, guess;
     int no_of_guess = 0;
     srand(time(NULL));
     printf("Welcome to the world of the Guessing Number..\n");
     random = rand() % 100 + 1;
-    do {
+    do{
         printf("Please enter your guess number (1 to 100): ");
         scanf("%d", &guess);
         no_of_guess++;
-        if (guess < random) {
+        if (guess < random){
             printf("Guess a larger number.\n");
         }
-        else if (guess > random) {
+        else if (guess > random){
             printf("Guess a smaller number.\n");
         }
-        else {
+        else{
             printf("?? Congratulations! You guessed the number in %d attempts.\n", no_of_guess);
         }
-    } while (guess != random);
+    } while(guess != random);
     printf("Thanks for playing!\n");
     printf("Developed by: Rudra Pratap Singh\n");
     return 0;
